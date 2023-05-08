b8d364912380b84eeeb70031ef24ce922b2e0f0a
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2023-05-08 23:03:16 +0100

    Add show more button

--- src/components/Cards/CardGallery.tsx
+++ src/components/Cards/CardGallery.tsx
@@ -1,8 +1,8 @@
-import React, { useMemo } from "react"
+import React, { useState, useEffect, useMemo, useCallback } from "react"
 import cardData from "../../../public/data/cardData.json"
 import Masonry from "react-masonry-css"
 
-import { useTheme, useColorModeValue, Card, Box, CardHeader, Heading, CardBody, Image, Flex, Stack, HStack, Text } from "@chakra-ui/react"
+import { useTheme, useColorModeValue, Card, Box, Heading, CardBody, Image, Flex, Stack, HStack, Text, Collapse, Button } from "@chakra-ui/react"
 
 import CardDescription from "./CardDescription"
 import CardLinks from "./CardLinks"
@@ -10,27 +10,32 @@ import CardImages from "./CardImages"
 import CardStatus from "./CardStatus"
 
 export default function CardGallery({ windowSize }) {
-    const customTheme = useTheme()
-    const cardBackground = useColorModeValue(customTheme.contentBackground.color.light, customTheme.contentBackground.color.dark)
-    const headingColor = useColorModeValue(customTheme.headingText.color.light, customTheme.headingText.color.dark)
-
-    const cardRefs = useMemo(() => cardData.map(() => React.createRef<HTMLDivElement>()), [])
-    const imageRefs = useMemo(
-        () => cardData.map((_, index) => Object.values(cardData[index].images || {}).map(() => React.createRef<HTMLImageElement>())),
-        []
-    )
-
     const breakpointCols = {
         default: 3,
         1400: 2,
         900: 1,
     }
 
+    const customTheme = useTheme()
+    const cardBackground = useColorModeValue(customTheme.contentBackground.color.light, customTheme.contentBackground.color.dark)
+    const headingColor = useColorModeValue(customTheme.headingText.color.light, customTheme.headingText.color.dark)
+
     const sortedCardData = [...cardData].sort((a, b) => {
         const dateA = new Date(a.startDate)
         const dateB = new Date(b.startDate)
         return dateB.getTime() - dateA.getTime()
     })
+
+    const [showMore, setShowMore] = useState(Array(sortedCardData.length).fill(false))
+    const toggleShowMore = useCallback((cardIndex: number) => {
+        setShowMore((prevState) => prevState.map((value, index) => (index === cardIndex ? !value : value)))
+    }, [])
+
+    const cardRefs = useMemo(() => sortedCardData.map(() => React.createRef<HTMLDivElement>()), [])
+    const imageRefs = useMemo(
+        () => sortedCardData.map((_, index) => Object.values(sortedCardData[index].images || {}).map(() => React.createRef<HTMLImageElement>())),
+        []
+    )
 
     return (
         <Box width="100%">
@@ -71,33 +76,59 @@ export default function CardGallery({ windowSize }) {
                                 </Box>
                                 <CardBody>
                                     <Box>
-                                        <CardDescription index={0} cardData={card} />
-                                    </Box>
-                                    <Box pt={5} mt={2}>
                                         <CardLinks cardData={card} />
                                     </Box>
+                                    <Box pt={5} mt={2}>
+                                        <CardDescription index={0} cardData={card} />
+                                    </Box>
                                 </CardBody>
-                                {Object.values(card?.images || {})
-                                    .slice(1)
-                                    .map((image, imageIndex, imageArray) => (
-                                        <>
-                                            <CardImages
-                                                key={imageIndex}
-                                                windowSize={windowSize}
-                                                cardIndex={cardIndex}
-                                                imageIndex={imageIndex}
-                                                image={image}
-                                                imageArray={imageArray}
-                                                cardRefs={cardRefs}
-                                                imageRefs={imageRefs}
-                                            />
-                                            {card?.description[imageIndex + 1] ? (
-                                                <CardBody>
-                                                    <CardDescription index={imageIndex} cardData={card} />
-                                                </CardBody>
-                                            ) : null}
-                                        </>
-                                    ))}
+                                {card?.images?.[1] && (
+                                    <CardImages
+                                        key={1}
+                                        windowSize={windowSize}
+                                        cardIndex={cardIndex}
+                                        imageIndex={2}
+                                        image={card?.images?.[1]}
+                                        imageArray={Object.values(card?.images || {})}
+                                        cardRefs={cardRefs}
+                                        imageRefs={imageRefs}
+                                        sortedCardData={sortedCardData}
+                                        showMore={showMore}
+                                    />
+                                )}
+                                {(card?.description?.[1] || card?.images?.[2]) && (
+                                    <>
+                                        <Collapse in={showMore[cardIndex]}>
+                                            {Object.values(card?.images || {})
+                                                .slice(2)
+                                                .map((image, imageIndex, imageArray) => (
+                                                    <>
+                                                        {card?.description[imageIndex + 1] ? (
+                                                            <CardBody>
+                                                                <CardDescription index={imageIndex} cardData={card} />
+                                                            </CardBody>
+                                                        ) : null}
+                                                        <CardImages
+                                                            key={imageIndex}
+                                                            windowSize={windowSize}
+                                                            cardIndex={cardIndex}
+                                                            imageIndex={imageIndex}
+                                                            image={image}
+                                                            imageArray={imageArray}
+                                                            cardRefs={cardRefs}
+                                                            imageRefs={imageRefs}
+                                                            sortedCardData={sortedCardData}
+                                                            showMore={showMore}
+                                                        />
+                                                    </>
+                                                ))}
+                                        </Collapse>
+                                        {/* TODO: Fix this by making a fully custom button using Box */}
+                                        <Button mt={0} sx={{ marginTop: "0 !important" }} onClick={() => toggleShowMore(cardIndex)}>
+                                            {showMore[cardIndex] ? "Show less" : "Show more"}
+                                        </Button>
+                                    </>
+                                )}
                             </Stack>
                         </Flex>
                     </Card>
--- src/components/Cards/CardImages.tsx
+++ src/components/Cards/CardImages.tsx
@@ -1,13 +1,33 @@
 import React, { useState, useEffect, useRef } from "react"
-import cardData from "../../../public/data/cardData.json"
-
 import { Box, Image } from "@chakra-ui/react"
 
-export default function CardImages({ windowSize, cardIndex, imageIndex, image, imageArray, cardRefs, imageRefs }) {
-    const [roundedCorners, setRoundedCorners] = useState(cardData.map((_, index) => Object.values(cardData[index].images || {}).map(() => false)))
+export default function CardImages({
+    windowSize,
+    cardIndex,
+    imageIndex,
+    image,
+    imageArray,
+    cardRefs,
+    imageRefs,
+    sortedCardData,
+    showMore,
+}: {
+    windowSize: { width: number; height: number }
+    cardIndex: number
+    imageIndex: number
+    image: { image: string; alt: string }
+    imageArray: Array<{ image: string; alt: string }>
+    cardRefs: Array<React.RefObject<HTMLDivElement>>
+    imageRefs: Array<Array<React.RefObject<HTMLImageElement>>>
+    sortedCardData: Array<any>
+    showMore: Array<boolean>
+}) {
+    const [roundedCorners, setRoundedCorners] = useState(
+        sortedCardData.map((_, index) => Object.values(sortedCardData[index].images || {}).map(() => false))
+    )
 
     useEffect(() => {
-        const newRoundedCorners = cardData.map((card, cardIndex) =>
+        const newRoundedCorners = sortedCardData.map((card, cardIndex) =>
             Object.values(card?.images || {})
                 .slice(1)
                 .map((_, imageIndex) => {
@@ -16,14 +36,14 @@ export default function CardImages({ windowSize, cardIndex, imageIndex, image, i
                     }
                     const cardWidth = cardRefs[cardIndex].current.offsetWidth
                     const imageWidth = imageRefs[cardIndex][imageIndex].current.offsetWidth
-                    return cardWidth - 1 > imageWidth
+                    return cardWidth > imageWidth
                 })
         )
 
         if (JSON.stringify(newRoundedCorners) !== JSON.stringify(roundedCorners)) {
             setRoundedCorners(newRoundedCorners)
         }
-    }, [windowSize.width, imageRefs, cardData, roundedCorners, cardRefs])
+    }, [windowSize.width, imageRefs, sortedCardData, roundedCorners, cardRefs])
 
     return (
         <Box width="100%" display="flex" alignItems="center" justifyContent="center">
@@ -41,7 +61,13 @@ export default function CardImages({ windowSize, cardIndex, imageIndex, image, i
                         ? "30px"
                         : "0px"
                 }
-                borderBottomRadius={imageIndex === imageArray.length - 1 ? "0px" : roundedCorners[cardIndex][imageIndex] ? "30px" : "0px"}
+                borderBottomRadius={
+                    imageIndex === imageArray.length - 1 || (imageIndex === 2 && !showMore[cardIndex])
+                        ? "0px"
+                        : roundedCorners[cardIndex][imageIndex]
+                        ? "30px"
+                        : "0px"
+                }
             />
         </Box>
     )
