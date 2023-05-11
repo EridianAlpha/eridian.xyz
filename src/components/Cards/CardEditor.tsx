131a92b652546ddf6824d0cb32132ef1c871c491
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2023-05-11 21:14:36 +0100

    Add error message to id input

--- src/components/Cards/CardEditor.tsx
+++ src/components/Cards/CardEditor.tsx
@@ -19,105 +19,90 @@ import {
     Textarea,
 } from "@chakra-ui/react"
 
-// TODO: Make the renderInput function this parent function
-// TODO: Remove InputLabel as a separate component and just use it directly in renderInputElement
-const renderInputs = (cardEditorData) => {
-    if (!cardEditorData) {
-        return { inputs: [], inputRefs: new Map() }
-    }
-
-    const inputRefs = new Map()
-
-    const renderInput = (key, value, path) => {
-        const renderInputElement = (elementType, ref) => {
-            inputRefs.set(path, ref)
-            return (
-                <>
-                    <InputLabel key={path} htmlFor={path}>
-                        {path}
-                    </InputLabel>
-                    {React.cloneElement(elementType, {
-                        key: path,
-                        ref: ref,
-                        placeholder: `Add ${key}...`,
-                        defaultValue: value,
-                    })}
-                </>
-            )
-        }
-
-        if (typeof value === "object") {
-            return Object.entries(value).map(([nestedKey, nestedValue]) =>
-                // If it's an object, loop through the children again using this same function
-                renderInput(nestedKey, nestedValue, path ? `${path}.${nestedKey}` : nestedKey)
-            )
-        } else {
-            if (key === "id") {
-                return renderInputElement(<Input disabled={true} />, React.createRef<HTMLInputElement>())
-            } else if (key === "summary" || path.includes("description")) {
-                return renderInputElement(<Textarea />, React.createRef<HTMLTextAreaElement>())
-            } else {
-                return renderInputElement(<Input />, React.createRef<HTMLInputElement>())
-            }
-        }
-    }
-
-    const InputLabel = ({ children, htmlFor }) => {
-        const customTheme = useTheme()
-        const labelColor = useColorModeValue(customTheme.headingText.color.light, customTheme.headingText.color.dark)
-        return (
-            <Text as="label" htmlFor={htmlFor} fontSize="lg" fontWeight="bold" color={labelColor} mb="8px" mt="16px" display="block">
-                {children}
-            </Text>
-        )
-    }
-
-    const inputs = Object.entries(cardEditorData).map(([key, value]) => renderInput(key, value, key))
-    return { inputs, inputRefs }
-}
-
-const getUpdatedCardData = (cardData, inputRefs, cardEditorData) => {
-    const updatedCardData = cardData.map((card) => {
-        if (card.id === cardEditorData?.id) {
-            let updatedCard = { ...card }
-
-            inputRefs.forEach((inputRef, key) => {
-                const inputValue = inputRef?.current?.value
-                const originalValue = key.split(".").reduce((obj, k) => (obj && obj[k] !== undefined ? obj[k] : null), card)
-
-                if (inputValue !== originalValue) {
-                    set(updatedCard, key, inputValue)
-                }
-            })
-            return updatedCard
-        }
-        return card
-    })
-    return updatedCardData
-}
-
-const updateCardData = (cardData, inputRefs, cardEditorData): Promise<void> => {
-    return new Promise<void>(async (resolve, reject) => {
-        const updatedCardData = getUpdatedCardData(cardData, inputRefs, cardEditorData)
-
-        try {
-            const response = await axios.post("/api/updateData", updatedCardData)
-            console.log(response.data.message)
-            resolve()
-        } catch (error) {
-            console.error("Error updating data:", error)
-            reject()
-        }
-    })
-}
-
 export default function CardEditor({ windowSize, isOpen, onClose, cardEditorData, cardData }) {
-    const { inputs, inputRefs } = renderInputs(cardEditorData)
-
     const toast = useToast()
 
     const customTheme = useTheme()
     const contentBackground = useColorModeValue(customTheme.contentBackground.color.light, customTheme.contentBackground.color.dark)
+    const labelColor = useColorModeValue(customTheme.headingText.color.light, customTheme.headingText.color.dark)
+
+    const renderInputs = (cardEditorData) => {
+        if (!cardEditorData) {
+            return { inputs: [], inputRefs: new Map() }
+        }
+
+        const inputRefs = new Map()
+        const renderInput = (key, value, path) => {
+            const renderInputElement = (elementType, ref) => {
+                inputRefs.set(path, ref)
+                return (
+                    <>
+                        <Text
+                            as="label"
+                            key={path}
+                            htmlFor={path}
+                            fontSize="lg"
+                            fontWeight="bold"
+                            color={labelColor}
+                            mb="8px"
+                            mt="16px"
+                            display="block"
+                        >
+                            {path}
+                        </Text>
+                        {React.cloneElement(elementType, {
+                            key: path,
+                            ref: ref,
+                            placeholder: `Add ${key}...`,
+                            defaultValue: value,
+                        })}
+                    </>
+                )
+            }
+
+            if (typeof value === "object") {
+                return Object.entries(value).map(([nestedKey, nestedValue]) =>
+                    // If it's an object, loop through the children again using this same function
+                    renderInput(nestedKey, nestedValue, path ? `${path}.${nestedKey}` : nestedKey)
+                )
+            } else {
+                if (key === "id") {
+                    return renderInputElement(<Input disabled={true} />, React.createRef<HTMLInputElement>())
+                } else if (key === "summary" || path.includes("description")) {
+                    return renderInputElement(<Textarea />, React.createRef<HTMLTextAreaElement>())
+                } else {
+                    return renderInputElement(<Input />, React.createRef<HTMLInputElement>())
+                }
+            }
+        }
+
+        const inputs = Object.entries(cardEditorData).map(([key, value]) => renderInput(key, value, key))
+        return { inputs, inputRefs }
+    }
+    const { inputs, inputRefs } = renderInputs(cardEditorData)
+
+    const getUpdatedCardData = (cardData, inputRefs, cardEditorData) => {
+        const updatedCardData = cardData.map((card) => {
+            if (card.id === cardEditorData?.id) {
+                let updatedCard = { ...card }
+
+                inputRefs.forEach((inputRef, key) => {
+                    if (key === "id" && inputRef?.current?.value !== card.id) {
+                        throw new Error("Updating 'id' is not allowed")
+                    }
+                    const inputValue = inputRef?.current?.value
+                    const originalValue = key.split(".").reduce((obj, k) => (obj && obj[k] !== undefined ? obj[k] : null), card)
+
+                    if (inputValue !== originalValue) {
+                        set(updatedCard, key, inputValue)
+                    }
+                })
+                return updatedCard
+            }
+            return card
+        })
+        return updatedCardData
+    }
 
     return (
         <Modal closeOnOverlayClick={false} onClose={onClose} isOpen={isOpen} scrollBehavior="inside">
@@ -130,7 +115,7 @@ export default function CardEditor({ windowSize, isOpen, onClose, cardEditorData
                     <Button
                         onClick={async () => {
                             try {
-                                await updateCardData(cardData, inputRefs, cardEditorData)
+                                await axios.post("/api/updateData", getUpdatedCardData(cardData, inputRefs, cardEditorData))
                                 onClose()
                             } catch (error) {
                                 toast({
@@ -138,7 +123,7 @@ export default function CardEditor({ windowSize, isOpen, onClose, cardEditorData
                                     status: "error",
                                     isClosable: true,
                                     position: "top",
-                                    description: error || "Unknown error 😞 Please try again later.",
+                                    description: error.message || "Unknown error 😞 Please try again later.",
                                     duration: 3000,
                                 })
                                 console.error("Error saving data:", error)
