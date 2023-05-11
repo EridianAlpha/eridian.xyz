7856cecff0ca4b7b2dc2c3d8c1348fdeb52b1fb7
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2023-05-11 15:01:59 +0100

    Add error message on editor save error

--- src/components/Cards/CardEditor.tsx
+++ src/components/Cards/CardEditor.tsx
@@ -3,6 +3,7 @@ import axios from "axios"
 
 import {
     useTheme,
+    useToast,
     useColorModeValue,
     Button,
     Modal,
@@ -16,30 +17,34 @@ import {
     Input,
 } from "@chakra-ui/react"
 
-const updateCardData = (cardData, cardEditorData, input1Ref) => {
-    if (input1Ref?.current?.value != cardEditorData?.name) {
-        const updatedCardData = cardData.map((card) => {
-            if (card.name === cardEditorData?.name) {
-                return { ...card, name: input1Ref?.current.value }
-            }
-            return card
-        })
+const updateCardData = (cardData, cardEditorData, input1Ref): Promise<void> => {
+    return new Promise<void>(async (resolve, reject) => {
+        if (input1Ref?.current?.value != cardEditorData?.name) {
+            const updatedCardData = cardData.map((card) => {
+                if (card.name === cardEditorData?.name) {
+                    return { ...card, name: input1Ref?.current.value }
+                }
+                return card
+            })
 
-        // Send a POST request to the updateData API route
-        axios
-            .post("/api/updateData", updatedCardData)
-            .then((response) => {
+            try {
+                const response = await axios.post("/api/updateData", updatedCardData)
                 console.log(response.data.message)
-            })
-            .catch((error) => {
+                resolve()
+            } catch (error) {
                 console.error("Error updating data:", error)
-            })
-    }
+                reject()
+            }
+        } else {
+            resolve()
+        }
+    })
 }
 
 export default function CardEditor({ windowSize, isOpen, onClose, cardEditorData, cardData }) {
-    // const btnRef = React.useRef(null)
-    const input1Ref = React.useRef(null)
+    const input1Ref = useRef(null)
+
+    const toast = useToast()
 
     const customTheme = useTheme()
     const contentBackground = useColorModeValue(customTheme.contentBackground.color.light, customTheme.contentBackground.color.dark)
@@ -55,10 +60,21 @@ export default function CardEditor({ windowSize, isOpen, onClose, cardEditorData
                 </ModalBody>
                 <ModalFooter>
                     <Button
-                        onClick={() => {
-                            updateCardData(cardData, cardEditorData, input1Ref)
-                            // TODO: Add a check here to only close if it successfully saves
-                            onClose()
+                        onClick={async () => {
+                            try {
+                                await updateCardData(cardData, cardEditorData, input1Ref)
+                                onClose()
+                            } catch (error) {
+                                toast({
+                                    title: "Error saving data",
+                                    status: "error",
+                                    isClosable: true,
+                                    position: "top",
+                                    description: error || "Unknown error 😞 Please try again later.",
+                                    duration: 3000,
+                                })
+                                console.error("Error saving data:", error)
+                            }
                         }}
                     >
                         Save
