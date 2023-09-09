540e49e0f6577a162c243d6e84164b5790fa406e
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2023-09-09 14:40:01 +0100

    Update data

--- src/components/Cards/CardLinks.tsx
+++ src/components/Cards/CardLinks.tsx
@@ -3,7 +3,7 @@ import NextLink from "next/link"
 import { useTheme, useColorModeValue, Text, Flex, Link, Box } from "@chakra-ui/react"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faGithub, faTwitter, faDiscord } from "@fortawesome/free-brands-svg-icons"
+import { faGithub, faXTwitter, faDiscord } from "@fortawesome/free-brands-svg-icons"
 import { faLink } from "@fortawesome/free-solid-svg-icons"
 
 interface LinkObject {
@@ -26,7 +26,7 @@ const CardLink = ({ link, index, backgroundColor, linkHoverColor }) => {
             >
                 <Flex alignItems={"center"} py={1} px={2}>
                     <FontAwesomeIcon
-                        icon={link?.type == "twitter" ? faTwitter : link?.type == "github" ? faGithub : link?.type == "discord" ? faDiscord : faLink}
+                        icon={link?.type == "twitter" ? faXTwitter : link?.type == "github" ? faGithub : link?.type == "discord" ? faDiscord : faLink}
                         size={"lg"}
                     />
                     <Text pl={2} pr={1}>
