a35f3362b8d8191ee27f6367defcd2981de3a39b
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-09-29 17:16:13 +0100

    Add app title to header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,11 @@ export default function Document() {
             <Head>
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/Eridian.png" />
+                <link rel="apple-touch-icon" href="/Eridian.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/Eridian.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/Eridian.png" />
+                <meta name="apple-mobile-web-app-title" content="Eridian.xyz" />
+                <meta name="application-name" content="Eridian.xyz" />
             </Head>
             <body>
                 <ColorModeScript initialColorMode="dark" />
